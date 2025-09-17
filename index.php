a29878200db031bb0021a7ddb00c1a48622cb648
avinashdemo14 <avinashdemo14@gmail.com>
2025-09-17 14:08:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<h2 style="font-weight: bold;">Sample Code For Practice Azure CI/CD Pipelines</h2>
+<?php echo "This is a php code dispalying current date-time ".date("Y-m-d H:i:s"); ?>
+<span>
+<h4 style="font-weight: bold;"> What is a portfolio ?</h4>
+<p>
+ 
+The portfolio can have the same purpose as a CV. Most CVs are written with text, while the portfolio is for showcasing, so it is visual with images and often more detailed than the CV.
+
+It is a place where you can show your work experience and showcase the projects that you are most proud of.
+
+Your online portfolio can be shared with its link to companies, hiring managers, and recruiters, so that they can notice you.
+
+It is about showing and giving others an understanding of who you are as a professional.
+</p>
+</span>
+
+<table>
+  <tr>
+    <th>Month</th>
+    <th>Savings</th>
+  </tr>
+  <tr>
+    <td>January</td>
+    <td>$100</td>
+  </tr>
+  <tr>
+    <td>February</td>
+    <td>$80</td>
+  </tr>
+</table>
+
+</body>
+</html>
