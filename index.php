2da227745d798df7ebcc22cdc345d7b5bd3dfd8f
avinashdemo14 <avinashdemo14@gmail.com>
2025-09-17 14:10:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ table, th, td {
 <h2 style="font-weight: bold;">Sample Code For Practice Azure CI/CD Pipelines</h2>
 <?php echo "This is a php code dispalying current date-time ".date("Y-m-d H:i:s"); ?>
 <span>
-<h4 style="font-weight: bold;"> What is a portfolio ?</h4>
+<h4 style="font-weight: bold;"> What is a portfolio ??</h4>
 <p>
  
 The portfolio can have the same purpose as a CV. Most CVs are written with text, while the portfolio is for showcasing, so it is visual with images and often more detailed than the CV.
@@ -42,3 +42,4 @@ It is about showing and giving others an understanding of who you are as a profe
 
 </body>
 </html>
+
